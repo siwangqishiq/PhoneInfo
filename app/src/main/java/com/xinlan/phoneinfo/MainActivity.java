e87201c544c3a51320b811aa023c7518875519a2
yixin.panyi <panyi@yixin.im>
2019-09-03 11:00:02 +0800

    获取设备唯一标识方法修改

--- app/src/main/java/com/xinlan/phoneinfo/MainActivity.java
+++ app/src/main/java/com/xinlan/phoneinfo/MainActivity.java
@@ -4,11 +4,13 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,7 +21,7 @@ import java.net.NetworkInterface;
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mAndroidIdText;
     private Button mCopyAndroidBtn;
@@ -52,12 +54,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mCopyMacBtn.setOnClickListener(this);
 
         getPhoneInfo();
+        Toast.makeText(this, "是否开启了开发者模式" + Utils.isDevMode(this), Toast.LENGTH_SHORT).show();
     }
 
 
     private void getPhoneInfo() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this , new String[]{Manifest.permission.READ_PHONE_STATE},100);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 100);
             return;
         }
 
@@ -65,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @SuppressLint("MissingPermission")
-    private void doGetPhoneInfo(){
+    private void doGetPhoneInfo() {
         mAndroidId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
         mAndroidIdText.setText(mAndroidId);
 
@@ -76,15 +79,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mMacText.setText(mMac);
     }
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-        if(requestCode == 100)
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 100)
             doGetPhoneInfo();
     }
 
     @SuppressLint("MissingPermission")
     public static String getMachineImei(Context context) {
+        String imei = null;
         TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
-         String imei = telephonyManager.getDeviceId();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            imei = telephonyManager.getImei();
+        } else {
+            imei = telephonyManager.getDeviceId();
+        }
+
+        if (TextUtils.isEmpty(imei)) {//imei没有获得
+            imei = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
         return imei;
     }
 
@@ -100,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 StringBuilder res1 = new StringBuilder();
                 for (byte b : macBytes) {
-                    res1.append(String.format("%02X:",b));
+                    res1.append(String.format("%02X:", b));
                 }
 
                 if (res1.length() > 0) {
@@ -116,18 +128,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.copy_androidid:
-                Utils.copy(mAndroidId , this);
-                Toast.makeText(this, "androidid 拷贝到剪切板成功!",Toast.LENGTH_SHORT).show();
+                Utils.copy(mAndroidId, this);
+                Toast.makeText(this, "androidid 拷贝到剪切板成功!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.copy_imei:
-                Utils.copy(mIMEI , this);
-                Toast.makeText(this, "IMEI 拷贝到剪切板成功!",Toast.LENGTH_SHORT).show();
+                Utils.copy(mIMEI, this);
+                Toast.makeText(this, "IMEI 拷贝到剪切板成功!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.copy_mac:
-                Utils.copy(mMac , this);
-                Toast.makeText(this, "mac 拷贝到剪切板成功!",Toast.LENGTH_SHORT).show();
+                Utils.copy(mMac, this);
+                Toast.makeText(this, "mac 拷贝到剪切板成功!", Toast.LENGTH_SHORT).show();
                 break;
         }
 
--- app/src/main/java/com/xinlan/phoneinfo/Utils.java
+++ app/src/main/java/com/xinlan/phoneinfo/Utils.java
@@ -1,7 +1,12 @@
 package com.xinlan.phoneinfo;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.text.ClipboardManager;
+import android.text.TextUtils;
 
 /**
  * Created by panyi on 2018/3/29.
@@ -12,5 +17,36 @@ public class Utils {
         // 得到剪贴板管理器
         ClipboardManager cmb = (ClipboardManager)context.getSystemService(Context.CLIPBOARD_SERVICE);
         cmb.setText(content.trim());
+    }
+
+    /**
+     * 是否开启了开发者模式
+     * @param context
+     * @return
+     */
+    public static boolean isDevMode(Context context){
+        return (Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.ADB_ENABLED, 0) > 0);
+    }
+
+    /**
+     * 获取设备唯一标识
+     *
+     * @param context
+     * @return
+     */
+    @SuppressLint("MissingPermission")
+    public static String getMachineImei(Context context) {
+        String imei = null;
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            imei = telephonyManager.getImei();
+        } else {
+            imei = telephonyManager.getDeviceId();
+        }
+
+        if (TextUtils.isEmpty(imei)) {//imei没有获得
+            imei = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
+        return imei;
     }
 }
